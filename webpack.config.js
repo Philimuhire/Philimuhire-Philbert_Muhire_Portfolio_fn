e2557ed1c5d60392d7c3f264297d6087199305be
Philimuhire <philimuhire@gmail.com>
2024-09-08 08:31:55 +0200

    Initialized the project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'], 
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+  },
+  mode: 'development',
+};
